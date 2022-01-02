640cec9b563bae8d2326f76cee55d02159fd2813
salkmon <66316046+salkmon@users.noreply.github.com>
2022-01-02 18:28:01 +0800

    Files from tile editor

---
+++ SamsungProject/core/levels/normal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="water" tilewidth="20" tileheight="20" tilecount="24" columns="6">
+ <image source="samsungsprites/water.png" width="120" height="80"/>
+</tileset>
