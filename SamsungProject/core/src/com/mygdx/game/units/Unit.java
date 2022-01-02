93f7c2717b2a899cebebb6c0420fa30f28d7c45e
LavraSource <90943312+LavraSource@users.noreply.github.com>
2022-01-02 18:33:14 +0800

    Update Unit.java

--- SamsungProject/core/src/com/mygdx/game/units/Unit.java
+++ SamsungProject/core/src/com/mygdx/game/units/Unit.java
@@ -4,8 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 
 public abstract class Unit {
     // Every tile spends move points from the unit
+    int[] pos;
     int humanAmount;
-    int health;
     int damage;
 
     Texture sprite;
