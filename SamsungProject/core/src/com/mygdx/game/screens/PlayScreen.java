43ca822ee6d1f2174d5ba54ecbcefad319896e3f
salkmon <66316046+salkmon@users.noreply.github.com>
2022-01-04 18:46:41 +0800

    Added play screen

---
+++ SamsungProject/core/src/com/mygdx/game/screens/PlayScreen.java
@@ -0,0 +1,56 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.MyGdxGame;
+
+public class PlayScreen implements Screen {
+    private MyGdxGame game;
+    Texture texture;
+
+    public PlayScreen(MyGdxGame game) {
+        this.game = game;
+        texture = new Texture("grass.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 100, 100);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
